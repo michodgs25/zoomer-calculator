399072953518fefcf36861488dec5e199ef3c6f7
michodgs25 <michodgs@gmail.com>
2021-06-09 13:58:10 +0000

    Create add, divide, subtract and multiple functions

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
 def calculator(a, b):
-    print(f"CALCULATING {a} + {b}")
+    print(f"CALCULATING {a}, {b}")
+    return a, b
+
+
+# create add function, return additional sum
+def add(a, b):
+    print(f"ADDING {a} + {b}")
     return a + b
+
+
+# create subtract function, return subtracted sum
+def subtract(a, b):
+    print(f"SUBTRACT {a} - {b}")
+    return a - b
+
+
+# create division function, return division sum
+def divide(a, b):
+    print(f"DIVIDE {a} / {b}")
+    return a / b
+
+
+# create multiply function, return multiple sum
+def multiple(a, b):
+    print(f"MULTIPLE {a} * {b}")
+    return a * b
+ 
